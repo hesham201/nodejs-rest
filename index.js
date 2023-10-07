e16b439e274137b6b452b52b933211eff9d5bdb1
hesham112 <107617341+hesham201@users.noreply.github.com>
2023-10-07 16:01:05 +0500

    dxqed

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  console.log("in middleware");
+const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-users", (req, res, next) => {
+  //   console.log("in another user middleware");
+  res.send(
+    "<form action='/users' method='POST'><input type='text' name='title'/><button type='submit'>submit</button></form>"
+  );
+  //   next();
 });
-const server = http.createServer(app);
-server.listen(4500);
+app.use("/users", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+  //   next();
+});
+app.use("/", (req, res, next) => {
+  console.log("in another middleware");
+  res.send("<h1>hello from express</h1>");
+  //   next();
+});
+app.listen(4500);
